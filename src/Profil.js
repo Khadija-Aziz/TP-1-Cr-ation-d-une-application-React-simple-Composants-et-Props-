8a53ca4ed867199f4f7bc510b68cb61a77ea1e56
Khadija-Aziz <khadijaaziz670@gmail.com>
2026-03-17 02:01:46 +0000

    Premier commit

---
+++ src/Profil.js
@@ -0,0 +1,27 @@
+// Profil.js
+import PropTypes from 'prop-types';
+
+function Profil({ utilisateur, taille }) {
+  return (
+    <div>
+      {utilisateur.nom}
+      <img src={utilisateur.photo} alt={utilisateur.nom} width={taille} height={taille} />
+      <p>{utilisateur.nom}</p>
+
+    </div>
+  );
+}
+
+Profil.propTypes = {
+  utilisateur: PropTypes.shape({
+    nom: PropTypes.string.isRequired,
+    photo: PropTypes.string.isRequired,
+  }).isRequired,
+  taille: PropTypes.number,
+};
+
+Profil.defaultProps = {
+  taille: 100,
+};
+
+export default Profil;
